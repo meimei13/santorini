f6d4d31797a63e57c9b5aacc57d2b15f27fb9468
Mei <mei@lemontart.me>
2021-05-10 00:43:37 -0700

    add logic for picking and moving spaces

--- lib/santorini/player.ex
+++ lib/santorini/player.ex
@@ -1,83 +1,217 @@
+#defmodule Win do
+#  alias Santorini.Player, as: Play
+#  alias Santorini.ReadWr, as: Json
+#
+#  defmacro win?(board, player) do
+#    quote do: unquote(Play.winning_move(board, Play.possible_moves(board, player))) != []
+#  end
+#end
+#
 defmodule Santorini.Player do
   @moduledoc false
+  #  import Win
   alias Santorini.ReadWr, as: Json
 
-  # takes a board struct, the space of the current worker, and a new place to move to
-  def move(board, cur, new) do
-    try do
-      play1 = Enum.fetch!(Json.players(board), 0)
-      play2 = Enum.fetch!(Json.players(board), 1)
-    rescue
-      Enum.OutOfBoundsError -> "For some reason, there aren't two players on the board!"
-    end
-
-    play1 = Enum.map(play1, fn 
-      coord when coord == cur -> new
+  # xc & yc = current coordinates
+  # xn & yn = new coordinates
+  def move(board, {xc, yc}, {xn, yn}) do
+    p1 = player1(board)
+    p2 = player2(board)
+  
+    
+    p1 = Enum.map(p1, fn 
+      coord when coord == [xc, yc] -> [xn, yn]
       coord -> coord
     end)
 
-    play2 = Enum.map(play2, fn 
-      coord when coord == cur -> new
+    p2 = Enum.map(p2, fn 
+      coord when coord == [xc, yc] -> [xn, yn]
       coord -> coord
     end)
 
-    players = [play1, play2]
+    players = [p1, p2]
 
     %Board{
       players: players,
       spaces: board.spaces,
-      turn: board.turn
+      turn: board.turn + 1
     }
   end
 
-  # If there is no first player, make the first move
-  def player1(board) do
-    try do
-      Enum.fetch!(Json.players(board), 0)
-    rescue
-      co1 = Enum.random(1..5)
-      co2 = Enum.random(1..5)
-      co3 = Enum.random(bound(co1-2)..bound(co1+2))
-      co4 = Enum.random(bound(co2-2)..bound(co2+2))
-      [[co1,co2],[co3,co4]]
+  # take a random move from a list of new moves
+  def move(board, cur, new) do
+    n = Enum.random(new)
+    move(board, List.to_tuple(cur), List.to_tuple(n))
+  end
+
+  # make the first move
+  # don't move more than two spaces from the first guy
+  def first_movep1(board) do
+    co1 = Enum.random(1..5)
+    co2 = Enum.random(1..5)
+    co3 = Enum.random((bound(co1-2)..bound(co1+2) -- [co1, co2]))
+    co4 = Enum.random((bound(co1-2)..bound(co1+2) -- [co1, co2]))
+    players = [
+      [
+        [co1, co2],
+        [co3, co4]
+      ],
+      [[],[]]
+    ]
+
+    %Board{
+      players: players,
+      spaces: board.spaces,
+      turn: board.turn + 1
+    }
+  end
+
+  # make the first move for p2
+  # don't move more than two spaces from the first guy or the first player
+  # (guy in this context is the first worker p2 puts down)
+  def first_movep2(board) do
+    p1 = player1(board)
+    co1 = Enum.random(for i <- p1, j <- i, do: bound(j + 1))
+    co2 = Enum.random(for i <- p1, j <- i, do: bound(j - 1))
+    co3 = Enum.random((bound(co1-2)..bound(co1+2) -- [co1, co2]))
+    co4 = Enum.random((bound(co1-2)..bound(co1+2) -- [co1, co2]))
+    players = [
+      p1,
+      [
+        [co1, co2],
+        [co3, co4]
+      ]
+    ]
+
+    %Board{
+      players: players, 
+      spaces: board.spaces, 
+      turn: board.turn + 1
+    }
+  end
+
+  def win?(board, player) do
+    m = possible_moves(board, player)
+    winning_move(board, m) != []
+  end
+
+  def pick_move(board) do
+    p1 = player1(board)
+    p2 = player2(board)
+
+    cond do
+      p1 == [[],[]] -> first_movep1(board)
+      win?(board, Enum.fetch!(p1, 0)) -> 
+        m = possible_moves(board, Enum.fetch!(p1, 0))
+        move(board, Enum.fetch!(p1, 0), winning_move(board, m))
+      win?(board, Enum.fetch!(p1, 1)) ->
+        m = possible_moves(board, Enum.fetch!(p1, 1))
+        move(board, Enum.fetch!(p1, 1), winning_move(board, m))
+      true -> _ = possible_moves(board, {0, 0})
     end
+
+    cond do
+      p2 == [[],[]] -> first_movep2(board)
+      win?(board, Enum.fetch!(p2, 0)) -> 
+        m = possible_moves(board, Enum.fetch!(p2, 0))
+        move(board, Enum.fetch!(p2, 0), winning_move(board, m))
+      win?(board, Enum.fetch!(p2, 1)) ->
+        m = possible_moves(board, Enum.fetch!(p2, 1))
+        move(board, Enum.fetch!(p1, 1), winning_move(board, m))
+      true -> r = Enum.fetch(p2, rand_guy())
+        move(board, r, possible_moves(board, r))
+    end
+
+    # make the first move if no move has been made (first or second player)
+    # case {player1(board),win?(board, Enum.fetch(player1(board),1))}  do
+    #   {[[],[]], _} -> first_movep1(board)
+    #   {x, true} -> 
+    #     m = possible_moves(board, Enum.fetch!(x, 0))
+    #     move(board, Enum.fetch!(x, 0), winning_move(board, m))
+    #   {x, true} ->
+    #     m = possible_moves(board, Enum.fetch!(x, 1))
+    #     move(board, Enum.fetch!(x, 1), winning_move(board, m))
+    #   # don't do anything with the first worker if he can't win
+    #   _ -> _ = possible_moves(board, {0, 0}) 
+    # end
+
+    # case {player2(board),win?(board, Enum.fetch(player1(board),1))}  do
+    #   {[[],[]], _} -> first_movep2(board)
+    #   {x, true} -> 
+    #     m = possible_moves(board, Enum.fetch!(x, 0))
+    #     move(board, Enum.fetch!(x, 0), winning_move(board, m))
+    #   {x, true} ->
+    #     m = possible_moves(board, Enum.fetch!(x, 1))
+    #     move(board, Enum.fetch!(x, 1), winning_move(board, m))
+    #   # don't do anything with the first worker if he can't win
+    #   {x, _} -> 
+    #     r = Enum.fetch!(x, rand_guy())
+    #     move(board, r, possible_moves(board, r))
+    # end
+  end
+
+  def rand_guy do
+    Enum.random([0, 1])
   end
 
   # set bounds for moves between 0-5 for the board size
   def bound(n) do
     case n do
-      x < 1 -> 1
-      x > 5 -> 5
+      x when x < 1 -> 1
+      x when x > 5 -> 5
       _ -> n
     end
   end
 
-  def player2(board) do
-    try do
-      play1 = Enum.fetch!(Json.players(board), 0)
-      Enum.fetch!(Json.players(board), 1)
-    rescue
-      if !play1 do
-        [[]]
-      end
+  def player1(board) do
+    case Enum.fetch(Json.players(board), 0) do
+      {:ok, pos} -> pos
+      {:error, _} -> [[],[]]
     end
   end
 
-  def first_move(board) do
-
+  # control flow - don't call this function until the first player has definitely moved :D  
+  def player2(board) do
+    case Enum.fetch(Json.players(board), 1) do
+      {:ok, pos} -> pos
+      {:error, _} -> [[],[]]
+    end
   end
 
-  def possible_moves(spaces, {x, y}) do
-    level = get_level(spaces, {x, y})
+  # spaces: current spaces on the board
+  # {x, y}: current position of guy for whom we are enumerating spaces
+  def possible_moves(board, {x, y}) do
+    # my level
+    level = get_level(board.spaces, {x, y})
+    d = delta({x, y})
+    players = Enum.flat_map(board.players, fn x -> x end)
 
+    # check levels are not == 4
+    moves = Enum.reject(d, fn [i, j] -> get_level(board.spaces, {i, j}) == 4 end) 
+    # check levels are not higher than one above -- will also remove out of bounds squares, because their level is nil
+    moves = Enum.reject(moves, fn [i, j] -> get_level(board.spaces, {i, j}) > level+1 end)
+    # remove spaces another player is already on
+    moves = moves -- players
+    moves
   end
 
-  def possible_moves([], {x, y}) do
+  def possible_moves(board, coord) do
+    possible_moves(board, List.to_tuple(coord))
+  end
 
+  def winning_move(board, moves) do
+    Enum.filter(moves, fn [i, j] -> get_level(board.spaces, {i, j}) == 3 end)
+  end
+
+  def delta({x, y}) do
+    s = for i <- [-1, 0, 1], j <- [-1, 0, 1], do: [i, j]
+    s = Enum.reject(s, fn z -> z == [0, 0] end)
+    Enum.map(s, fn [i, j] -> [i+x, j+y] end)
   end
 
   def get_level(spaces, {x, y}) do
-    Enum.at(spaces, x)
-    |>Enum.at(y)
+    # flatt coords start at 1 and not 0
+    Enum.at(spaces, x-1)
+    |>Enum.at(y-1)
   end
 end
--- test/santorini_test.exs
+++ test/santorini_test.exs
@@ -2,7 +2,7 @@ defmodule SantoriniTest do
   use ExUnit.Case
   doctest Santorini
   alias Santorini.ReadWr, as: Json
-
+  alias Santorini.Player, as: Play
 
   defp test_json() do
     # sigils for string
@@ -60,19 +60,70 @@ defmodule SantoriniTest do
     end
   end
 
-  describe "constraints on moves" do
-    test "enumerate all possible moves" do
+  describe "constraints on moves:" do
+    test "enumerate all possible moves for [2, 3]" do
       # possible moves for the given board for the player [2, 3]
       moves = [
-        [2, 2],
-        [1, 3],
         [1, 2],
+        [1, 3],
+        [1, 4],
+        [2, 2],
+        [2, 4],
         [3, 2],
         [3, 3],
         [3, 4],
-        [1, 4],
-        [2, 4]
       ]
+      assert Play.possible_moves(correct(), {2, 3}) == moves
+    end
+
+    test "enumerate all possible moves for [2, 5] guy on the end" do
+      moves = [
+        [1, 4],
+        [2, 4],
+      ]
+      assert Play.possible_moves(correct(), {2, 5}) == moves
+    end
+
+    test "enumerate all possible moves for [3, 5]" do 
+      moves = [
+        [2, 4],
+        [4, 5]
+      ]
+      assert Play.possible_moves(correct(), {3, 5}) == moves
+    end
+
+    test "get correct winning move(s)" do
+      mv = Play.possible_moves(correct(), {2, 3}) 
+      assert Play.winning_move(correct(), mv) == [[3,4]]
+    end
+
+    test "get correct level" do
+      assert Play.get_level(Json.spaces(correct()), {1, 5}) == 2    
+    end
+  end
+
+  describe "player positions:" do
+    test "player 1 is in correct spot" do
+      assert Play.player1(correct()) == [[2,3], [4,4]]
+    end
+    
+    test "player 2 is in correct spot" do
+      assert Play.player2(correct()) == [[2,5], [3,5]]
+    end
+
+    test "moving a player moves them to correct spot" do
+      c = %Board{
+                players: [[[3, 4], [4, 4]], [[2, 5], [3, 5]]],
+                spaces: [
+                  [0, 0, 0, 0, 2],
+                  [1, 1, 2, 0, 0],
+                  [1, 0, 0, 3, 0],
+                  [0, 0, 3, 0, 0],
+                  [0, 0, 0, 1, 4]
+                ],
+                turn: 19
+              }
+      assert Play.move(correct(), {2,3}, {3,4}) == c
     end
   end
 end
